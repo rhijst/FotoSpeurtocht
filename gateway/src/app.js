d94b77f37c310aa245416130ee0e905afb699362
dannyvg <dannyvangasteren14@gmail.com>
2026-04-09 14:47:31 +0200

    added gui

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -5,6 +5,15 @@ require('dotenv').config();
 
 const app = express();
 
+//`CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
 /*
 Auth - Login
 */
--- start-all.bat
+++ start-all.bat
@@ -7,13 +7,14 @@ if exist "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe" (
     ; new-tab --title "register-service" cmd /k "cd /d "%ROOT%register-service" && docker compose up" ^
     ; new-tab --title "target-service"   cmd /k "cd /d "%ROOT%target-service" && docker compose up" ^
     ; new-tab --title "score-service"    cmd /k "cd /d "%ROOT%score-service" && docker compose up" ^
+    ; new-tab --title "gui"              cmd /k "cd /d "%ROOT%gui" && docker compose up" ^
     ; new-tab --title "gateway"          cmd /k "cd /d "%ROOT%gateway" && docker compose up"
 ) else (
-    start "minio" cmd /k "cd /d "%ROOT%minio" && docker compose up"
-    start "rabbitMQ" cmd /k "cd /d "%ROOT%rabbitmq" && docker compose up"
-    start "register-service" cmd /k "cd /d "%ROOT%register-service" && docker compose up"
+    start "minio"            cmd /k "cd /d "%ROOT%minio" && docker compose up"
+    start "rabbitMQ"         cmd /k "cd /d "%ROOT%rabbitmq" && docker compose up"
     start "register-service" cmd /k "cd /d "%ROOT%register-service" && docker compose up"
     start "target-service"   cmd /k "cd /d "%ROOT%target-service" && docker compose up"
     start "score-service"    cmd /k "cd /d "%ROOT%score-service" && docker compose up"
+    start "gui"              cmd /k "cd /d "%ROOT%gui" && docker compose up"
     start "gateway"          cmd /k "cd /d "%ROOT%gateway" && docker compose up"
 )
