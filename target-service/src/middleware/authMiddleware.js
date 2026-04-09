2ee410ce995cc3c59bc4a203d872615fc67e6281
Thijs Teerhuis <thijs@teerhuis.eu>
2026-04-09 11:12:13 +0200

    Removed duble JWT check
    JWT was checked in gatway and target service

--- target-service/src/middleware/authMiddleware.js
+++ target-service/src/middleware/authMiddleware.js
@@ -1,26 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  const userId = req.headers['x-user-id'];
+  const role = req.headers['x-user-role'];
 
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "No token provided" });
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 
-  const token = authHeader.split(" ")[1];
-
-  try {
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = decoded;
-
-    next();
-
-  } catch (err) {
-
-    return res.status(401).json({ error: "Invalid token" });
-
-  }
+  req.user = { userId, role };
+  next();
 };
