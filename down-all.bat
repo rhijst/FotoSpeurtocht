778c7736e2204e18243da8d4e72fdfddb4ee8330
Thijs Teerhuis <thijs@teerhuis.eu>
2026-04-11 16:56:27 +0200

    Create new script to delete all contianers

---
+++ down-all.bat
@@ -0,0 +1,26 @@
+@echo off
+set ROOT=%~dp0
+
+if exist "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe" (
+    wt new-tab --title "minio"           cmd /k "cd /d "%ROOT%minio" && docker compose down -v" ^
+    ; new-tab --title "mail-service"     cmd /k "cd /d "%ROOT%mail-service" && docker compose down -v" ^
+    ; new-tab --title "clock-service"    cmd /k "cd /d "%ROOT%clock-service" && docker compose down -v" ^
+    ; new-tab --title "register-service" cmd /k "cd /d "%ROOT%register-service" && docker compose down -v" ^
+    ; new-tab --title "rabbitMQ"         cmd /k "cd /d "%ROOT%rabbitmq" && docker compose down -v" ^
+    ; new-tab --title "join-service"     cmd /k "cd /d "%ROOT%join-service" && docker compose down -v" ^
+    ; new-tab --title "target-service"   cmd /k "cd /d "%ROOT%target-service" && docker compose down -v" ^
+    ; new-tab --title "score-service"    cmd /k "cd /d "%ROOT%score-service" && docker compose down -v" ^
+    ; new-tab --title "gui"              cmd /k "cd /d "%ROOT%gui" && docker compose down -v" ^
+    ; new-tab --title "gateway"          cmd /k "cd /d "%ROOT%gateway" && docker compose down -v"
+) else (
+    start "minio"            cmd /k "cd /d "%ROOT%minio" && docker compose down -v"
+    start "mail-service"     cmd /k "cd /d "%ROOT%mail-service" && docker compose down -v"
+    start "clock-service"    cmd /k "cd /d "%ROOT%clock-service" && docker compose down -v"
+    start "register-service" cmd /k "cd /d "%ROOT%register-service" && docker compose down -v"
+    start "rabbitMQ"         cmd /k "cd /d "%ROOT%rabbitmq" && docker compose down -v"
+    start "join-service"     cmd /k "cd /d "%ROOT%join-service" && docker compose down -v"
+    start "target-service"   cmd /k "cd /d "%ROOT%target-service" && docker compose down -v"
+    start "score-service"    cmd /k "cd /d "%ROOT%score-service" && docker compose down -v"
+    start "gui"              cmd /k "cd /d "%ROOT%gui" && docker compose down -v"
+    start "gateway"          cmd /k "cd /d "%ROOT%gateway" && docker compose down -v"
+)
